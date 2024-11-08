1c65f25919ff4805f16dc7fe58ead6a494c42963
Mosses Ross <mosasross@gmail.com>
2024-11-08 11:23:38 +0530

    Create Inexperienced_Project_Consultants.sql

---
+++ Inexperienced_Project_Consultants.sql
@@ -0,0 +1,4 @@
+SELECT pc.name, pc.custrecord_mos_pc_experience as exp, pc.custrecord_mos_pc_first_name as first_name, pc.custrecord_mos_pc_last_name as last_name, pc.custrecord_mos_pc_rate_per_hour as pay, pc.custrecord_mos_pc_phone as number
+	FROM customrecord_mos_pc pc
+		WHERE pc.custrecord_mos_pc_experience < 5
+			ORDER BY pc.custrecord_mos_pc_experience
