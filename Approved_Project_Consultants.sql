79b48767c02e34f155cd99f2cadf938a954a9bc3
Mosses Ross <mosasross@gmail.com>
2024-11-08 11:30:29 +0530

    Create Approved_Project_Consultants.sql

---
+++ Approved_Project_Consultants.sql
@@ -0,0 +1,4 @@
+SELECT pc.name, pc.custrecord_mos_pc_status as status, pc.custrecord_mos_pc_rate_per_hour as pay, pc.custrecord_mos_pc_phone as number
+	FROM customrecord_mos_pc pc
+		WHERE pc.custrecord_mos_pc_status = 3
+			ORDER BY pc.name
