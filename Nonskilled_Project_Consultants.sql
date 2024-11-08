cb605e63b589c47ee1f0ec9eb04295f9a4f0818b
Mosses Ross <mosasross@gmail.com>
2024-11-08 10:54:56 +0530

    Create Nonskilled_Project_Consultants.sql

---
+++ Nonskilled_Project_Consultants.sql
@@ -0,0 +1,4 @@
+SELECT pc.name, pc.custrecord_mos_pc_first_name as first_name, pc.custrecord_mos_pc_last_name as last_name, pc.custrecord_mos_pc_skill_level as skill, pc.custrecord_mos_pc_phone as number
+	FROM customrecord_mos_pc pc
+		WHERE pc.custrecord_mos_pc_skill_level = 3
+			ORDER BY pc.name
